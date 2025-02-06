3fc1f5d2c5c77b0105c3ef908062a730393161a1
PrinceAhbar <demarcogray81@gmail.com>
2025-02-05 21:32:23 -0500

    Added error messages and button disabled

---
+++ scripts/validation.js
@@ -0,0 +1,63 @@
+const showInputError = (formElement, inputElement, errorMsg) => {
+  const errorMsgElement = formElement.querySelector(
+    `#${inputElement.id}-error`
+  );
+  errorMsgElement.textContent = errorMsg;
+  inputElement.classList.add("modal__input_type_error");
+};
+
+const hideInputError = (formElement, inputElement, errorMsg) => {
+  const errorMsgElement = formElement.querySelector(
+    `#${inputElement.id}-error`
+  );
+  errorMsgElement.textContent = "";
+  inputElement.classList.remove("modal__input_type_error");
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  console.log(hasInvalidInput(inputList));
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(".modal__submit-button_invalid");
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove(".modal__submit-button_invalid");
+    buttonElement.disabled = false;
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
+  const buttonElement = formElement.querySelector(".modal__submit-button");
+
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
